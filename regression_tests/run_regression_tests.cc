eaab2d24fc8195b2f65ca3cd7c05ea636f18b41a
Wojciech Rembelski <ksaiwn@wp.pl>
2021-05-08 11:12:03 +0200

    Restructure cmake in order to handle tests automatically

--- regression_tests/run_regression_tests.cc
+++ regression_tests/run_regression_tests.cc
@@ -2,18 +2,16 @@
 #include <filesystem>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cerr << "Al least one argument needs to be passed!\n";
+  if (argc < 3) {
+    std::cerr << "Al least two arguments needs to be passed!\n";
     return -1;
   }
-  const std::string TESTING_DIRECTORY_ROOT = (argc == 2) ? argv[1] : argv[2];
-  if (argc == 2) {
-    std::filesystem::remove_all(TESTING_DIRECTORY_ROOT);
-  }
+  const std::string TESTING_DIRECTORY_ROOT = argv[1];
+  const std::string TESTFILE_LOCATION = argv[2];
+
+  std::filesystem::remove_all(TESTING_DIRECTORY_ROOT);
   std::filesystem::create_directory(TESTING_DIRECTORY_ROOT);
-  std::string test_location(TESTFILES_LOCATION + std::string(argv[1]));
 
   RegressionTestsParser parser(TESTING_DIRECTORY_ROOT);
-  auto result = parser.parseFile(test_location);
-  return result;
+  return parser.parseFile(TESTFILE_LOCATION);
 }
